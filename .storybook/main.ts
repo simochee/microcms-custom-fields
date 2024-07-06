70fda10a26c98a40c43fce92dfca22084bfa8fcc
simochee <lollipop@simochee.net>
2024-07-06 13:31:43 +0900

    ci: set base url

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
+const { BASE_URL = "/" } = process.env;
+
 const config: StorybookConfig = {
 	stories: ["../src/**/*.@(mdx|stories.tsx)"],
 	addons: [
@@ -7,6 +9,10 @@ const config: StorybookConfig = {
 		"@storybook/addon-onboarding",
 		"@storybook/addon-interactions",
 	],
+	viteFinal: (config) => {
+		config.base = BASE_URL;
+		return config;
+	},
 	framework: {
 		name: "@storybook/react-vite",
 		options: {},
